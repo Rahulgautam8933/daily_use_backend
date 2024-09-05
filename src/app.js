9fb0496d16fe4183830223382c464e5663026397
rahul <rahulsoftfix@gmail.com>
2024-09-05 12:20:09 +0530

    add

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 // app.js
 import express from 'express';
 import { PORT } from './constants.js';
+import userRoutes from './routes/userRoutes.js'; // Adjust path as necessary
+
 
 const app = express();
 
@@ -9,6 +11,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+// Use user routes
+app.use('/users', userRoutes);
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
