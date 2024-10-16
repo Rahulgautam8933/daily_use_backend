ad0075230003fe81d235a3dc205b19df5fbeb5e1
rahul <rahulsoftfix@gmail.com>
2024-10-16 14:55:51 +0530

    update

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ import express from "express"
 import cors from "cors"
 import cookiesParser from "cookie-parser"
 
-const app =express()
+const app = express()
 
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
 }))
-app.use(express.json({limit:"16kb"}));
-app.use(express.urlencoded({extended:true,limit:"16kb"}));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookiesParser());
 
@@ -17,9 +17,9 @@ app.use(cookiesParser());
 import userRouter from "./routes/user.routes.js";
 import CustomerRouter from "./routes/customer.routes.js";
 
-app.use("/api/v1/users",userRouter);
-app.use("/api/v1/customer",CustomerRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/customer", CustomerRouter);
 
 
 
-export {app}
+export { app }
